724856a87f72f28f64564b0ed3f24a251ec78805
minnica <minnica20@gmail.com>
2025-07-29 19:54:01 -0600

    feat(*) add initial structure

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,html}', './components/**/*.{js,ts,html,css}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
