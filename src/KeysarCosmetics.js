1f32eb03c77e7d9f3a75554830c41e71651f1e0d
minnica <minnica20@gmail.com>
2025-08-22 03:04:47 -0600

    feat(*) fix routes

--- src/KeysarCosmetics.js
+++ src/KeysarCosmetics.js
@@ -1,7 +1,7 @@
 import { LitElement, html } from 'lit';
 import './index.css';
-import '@web-components-features/feature-sales-management-crud/src/FeatureSalesManagementCrud.js';
-import '@web-components-features/feature-login/src/FeatureLogin.js';
+import '../node_modules/@web-components-features/feature-sales-management-crud/src/FeatureSalesManagementCrud.js';
+import '../node_modules/@web-components-features/feature-login/src/FeatureLogin.js';
 
 export class KeysarCosmetics extends LitElement {
   static get properties() {
