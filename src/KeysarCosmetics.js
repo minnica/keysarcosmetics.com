1c54c2ab2774fb43c6b6a348a8f83e36b19cddc2
minnica <minnica20@gmail.com>
2025-08-22 02:44:37 -0600

    feat(*) add features and ready to deploy

--- src/KeysarCosmetics.js
+++ src/KeysarCosmetics.js
@@ -1,28 +1,58 @@
 import { LitElement, html } from 'lit';
 import './index.css';
-import '@web-components-features/feature-sales-management-crud';
+import '@web-components-features/feature-sales-management-crud/src/FeatureSalesManagementCrud.js';
+import '@web-components-features/feature-login/src/FeatureLogin.js';
 
 export class KeysarCosmetics extends LitElement {
   static get properties() {
     return {
-      test: { type: String },
+      /**
+       * Indicates if the user is authenticated
+       * @type {Boolean}
+       * @default false
+       */
+      authenticated: {
+        type: Boolean,
+      },
     };
   }
 
   constructor() {
     super();
-    this.test = 'test';
+    this.authenticated = false;
   }
 
   createRenderRoot() {
     return this;
   }
 
+  /**
+   * Handles the successful login event.
+   * @param {CustomEvent} e - The event object.
+   * @private
+   */
+  _handleLoginSuccess(e) {
+    this.authenticated = e.detail?.authenticated;
+  }
+
+  /**
+   * Handles the logout event.
+   * @private
+   */
+  _handleLogout() {
+    this.authenticated = false;
+  }
+
   render() {
-    return html`<div class="bg-red-500">${this.test}</div>
-    <feature-sales-management-crud></feature-sales-management-crud>
+    return html`
+      ${this.authenticated
+        ? html`<feature-sales-management-crud
+            @nav-bar-logout="${e => this._handleLogout(e)}"
+          ></feature-sales-management-crud>`
+        : html`<feature-login
+            @feature-login-success="${e => this._handleLoginSuccess(e)}"
+          ></feature-login>`}
     `;
-   
   }
 }
 customElements.define('keysar-cosmetics', KeysarCosmetics);
